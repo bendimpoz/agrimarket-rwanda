f3ec292870dcef334128c0da8a64e5032ed32af6
Ivan <ivanniyonkuru@gmail.com>
2026-06-07 17:27:30 -0500

    fix: add neon url to prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: "postgresql://neondb_owner:npg_y7bkYm5exBaK@ep-summer-firefly-ap5od6y5-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
