54f779989949c4e1058deb805d61c5a8ddd235ff
Ivan <ivanniyonkuru@gmail.com>
2026-06-07 17:50:06 -0500

    fix: use DATABASE_URL env variable in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: "postgresql://neondb_owner:npg_y7bkYm5exBaK@ep-summer-firefly-ap5od6y5-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    url: process.env.DATABASE_URL ?? "postgresql://neondb_owner:npg_y7bkYm5exBaK@ep-summer-firefly-ap5od6y5-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
